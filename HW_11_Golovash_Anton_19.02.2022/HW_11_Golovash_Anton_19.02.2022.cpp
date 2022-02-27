d70c4199408b4a5963f3260889f93ecad0e2efbd
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-02-27 13:30:43 +0200

    Добавьте файлы проекта.

---
+++ HW_11_Golovash_Anton_19.02.2022/HW_11_Golovash_Anton_19.02.2022.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class PhoneBook
+{
+public:
+	PhoneBook(string phoneNumber, string surname, string initials)
+	{
+		this->phoneNumber = phoneNumber;
+		this->surname = surname;
+		this->initials = initials;
+	}
+	~PhoneBook()
+	{
+		delete this;
+	}
+	string phoneNumber { "default" };
+	string surname { "default" };
+	string initials { "default" };
+};
+
+
+int main()
+{
+	vector<PhoneBook> setPhoneBook
+	{
+		PhoneBook("+380956347984","Петров","С.П."),
+		PhoneBook("+380673596842","Самбуров","Р.Е."),
+		PhoneBook("+380669845632","Крайнев","И.Н."),
+		PhoneBook("","",""),
+		PhoneBook("","",""),
+		PhoneBook("","",""),
+		PhoneBook("","",""),
+		PhoneBook("","",""),
+		PhoneBook("","",""),
+		PhoneBook("","",""),
+	};
+
+	sort (setPhoneBook.begin(), setPhoneBook.end(), [](const PhoneBook& P1, const PhoneBook& P2)
+		{ return P1.surname < P2.surname; });
+
+	auto findIfResult = find_if (setPhoneBook.begin(), setPhoneBook.end(), [](const PhoneBook &P)
+		{
+			return P.surname == "Самбуров";
+		});
+
+	if (findIfResult == setPhoneBook.end())
+	{
+		cout << "Строка НЕ найдена" << endl;
+	}
+	else
+	{
+		cout << "Строка найдена" << endl;
+	}
+	return 0;
+}
